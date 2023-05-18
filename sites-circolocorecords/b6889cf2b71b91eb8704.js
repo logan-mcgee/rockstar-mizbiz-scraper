234e23706467443371c5e0f3c84b5bbea4a95070
logan <31740751+logan-mcgee@users.noreply.github.com>
2023-05-18 16:58:14 +0100

    remove circoloco as moving to new repo

--- sites-circolocorecords/b6889cf2b71b91eb8704.js
+++
@@ -1,7 +0,0 @@
-(self.webpackChunk_rockstargames_sites_circolocorecords = self.webpackChunk_rockstargames_sites_circolocorecords || []).push([
-  [774], {
-    3774: (e, s, c) => {
-      Promise.all([c.e(976), c.e(190)]).then(c.bind(c, 3190))
-    }
-  }
-]);
